b49027c27273da48fbf0a95f2964692aac685436
pos <pgousdal@users.noreply.github.com>
2026-09-08 19:10:02 +0200

    M3.1: expose ARexx message processing

--- src/arexx_amiga.h
+++ src/arexx_amiga.h
@@ -1,11 +1,17 @@
 #ifndef REXXTELNET_AREXX_AMIGA_H
 #define REXXTELNET_AREXX_AMIGA_H
 
+#include "arexx_dispatch.h"
+
 struct rt_arexx_port;
 
 struct rt_arexx_port *rt_arexx_port_open(void);
 void rt_arexx_port_close(struct rt_arexx_port *port);
 unsigned long rt_arexx_port_signal_mask(const struct rt_arexx_port *port);
 int rt_arexx_port_pending(const struct rt_arexx_port *port);
+int rt_arexx_port_process(struct rt_arexx_port *port,
+                          const struct rt_arexx_ops *ops,
+                          void *ctx,
+                          int *quit_requested);
 
 #endif
