0cd147b055a1fb11e1ec5a548b0f9e04a3b42898
pos <pgousdal@users.noreply.github.com>
2026-09-08 18:36:03 +0200

    M1: define portable transport contract

---
+++ src/transport.h
@@ -0,0 +1,23 @@
+#ifndef REXXTELNET_TRANSPORT_H
+#define REXXTELNET_TRANSPORT_H
+
+#include <stddef.h>
+
+struct rt_transport {
+    int socket_fd;
+    int connected;
+};
+
+void rt_transport_init(struct rt_transport *transport);
+int rt_transport_connect(struct rt_transport *transport,
+                         const char *host,
+                         unsigned short port);
+long rt_transport_send(struct rt_transport *transport,
+                       const unsigned char *data,
+                       size_t len);
+long rt_transport_recv(struct rt_transport *transport,
+                       unsigned char *data,
+                       size_t len);
+void rt_transport_disconnect(struct rt_transport *transport);
+
+#endif
