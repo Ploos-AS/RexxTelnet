4ebebf0c57eff72c7c4d650a951b5309842c509d
pos <pgousdal@users.noreply.github.com>
2026-09-08 18:49:56 +0200

    M2: add terminal core interface

---
+++ src/terminal.h
@@ -0,0 +1,42 @@
+#ifndef REXXTELNET_TERMINAL_H
+#define REXXTELNET_TERMINAL_H
+
+#include <stddef.h>
+
+#define RT_TERM_MAX_PARAMS 4
+
+struct rt_terminal {
+    unsigned short columns;
+    unsigned short rows;
+    unsigned short cursor_x;
+    unsigned short cursor_y;
+    unsigned char esc_state;
+    unsigned int params[RT_TERM_MAX_PARAMS];
+    unsigned char param_count;
+    unsigned int current_param;
+    int current_param_set;
+};
+
+typedef void (*rt_terminal_emit_cb)(void *ctx, unsigned char byte);
+typedef void (*rt_terminal_control_cb)(void *ctx,
+                                       unsigned char command,
+                                       const unsigned int *params,
+                                       unsigned char param_count);
+
+void rt_terminal_init(struct rt_terminal *term,
+                      unsigned short columns,
+                      unsigned short rows);
+
+void rt_terminal_feed(struct rt_terminal *term,
+                      const unsigned char *data,
+                      size_t len,
+                      rt_terminal_emit_cb on_text,
+                      rt_terminal_control_cb on_control,
+                      void *ctx);
+
+size_t rt_terminal_build_naws(unsigned short columns,
+                              unsigned short rows,
+                              unsigned char *output,
+                              size_t output_size);
+
+#endif
